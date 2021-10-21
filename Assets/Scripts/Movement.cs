e3457eadc99c077d583d7b436b2565acf7bc02b1
Eliott-Guignabaudet <92458791+Eliott-Guignabaudet@users.noreply.github.com>
2021-10-21 17:46:01 +0200

    Add pause menu and racasts
    Add a pause menu with just a resume button and raycast for interact with game objacts.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -14,66 +14,81 @@ public class Movement : MonoBehaviour
     float rotationX = 0;
     public float rotationSpeed = 2.0f;
     public float rotationXLimit = 45.0f;
+
+    public bool isPause = false;
+    public GameObject pauseMenu;
+
     void Start()
     {
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked ;
         characterController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
-
-        float speedZ = Input.GetAxis("Vertical");
-        float speedX = Input.GetAxis("Horizontal");
-        float speedY = moveDirection.y;
-
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (!isPause)
         {
-            isRunning = true;
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+
+            float speedZ = Input.GetAxis("Vertical");
+            float speedX = Input.GetAxis("Horizontal");
+            float speedY = moveDirection.y;
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                isRunning = true;
+            }
+            else
+            {
+                isRunning = false;
+            }
+
+            if (isRunning)
+            {
+                speedX *= runningSpeed;
+                speedZ *= runningSpeed;
+            }
+            else
+            {
+                speedX *= walkingSpeed;
+                speedZ *= walkingSpeed;
+            }
+
+            moveDirection = forward * speedZ + right * speedX;
+
+            if (Input.GetButton("Jump") && characterController.isGrounded)
+            {
+                moveDirection.y = jumpSpeed;
+            }
+            else
+            {
+                moveDirection.y = speedY;
+            }
+
+            if (!characterController.isGrounded)
+            {
+                moveDirection.y -= gravity * Time.deltaTime;
+            }
+
+
+            characterController.Move(moveDirection * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                isPause = true;
+                Cursor.visible = true;
+                Cursor.lockState = 0;
+                pauseMenu.SetActive(true);
+            }
+            
+            rotationX += -Input.GetAxis("Mouse Y") * rotationSpeed;
+            rotationX = Mathf.Clamp(rotationX, -rotationXLimit, rotationXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX,0,0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * rotationSpeed, 0);
         }
-        else
-        {
-            isRunning = false;
-        }
-
-        if (isRunning)
-        {
-            speedX *= runningSpeed;
-            speedZ *= runningSpeed;
-        }
-        else
-        {
-            speedX *= walkingSpeed;
-            speedZ *= walkingSpeed;
-        }
-
-        moveDirection = forward * speedZ + right * speedX;
-
-        if (Input.GetButton("Jump") && characterController.isGrounded)
-        {
-            moveDirection.y = jumpSpeed;
-        }
-        else
-        {
-            moveDirection.y = speedY;
-        }
-
-        if (!characterController.isGrounded)
-        {
-            moveDirection.y -= gravity * Time.deltaTime;
-        }
-
-
-        characterController.Move(moveDirection * Time.deltaTime);
-
-        rotationX += -Input.GetAxis("Mouse Y") * rotationSpeed;
-
-        rotationX = Mathf.Clamp(rotationX, -rotationXLimit, rotationXLimit);
-
-        playerCamera.transform.localRotation = Quaternion.Euler(rotationX,0,0);
-
-        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * rotationSpeed, 0);
+        
+        
     }
 }
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject player;
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        player.GetComponent<Movement>().isPause = false;
+    }
+}
