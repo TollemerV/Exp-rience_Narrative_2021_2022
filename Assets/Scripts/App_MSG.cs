716a51ec97c8e6a02d437d47366878e7d9d7681e
CM <corentin.mailler19@gmail.com>
2021-12-04 16:04:50 +0100

    add scroll view on app_msg and creation of chat screen

---
+++ Assets/Scripts/App_MSG.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class App_MSG : MonoBehaviour
+{
+    public void Open_Chat(string chatNameSelect)
+    {
+        SceneManager.LoadScene(chatNameSelect);
+    }
+
+    public void quitChat()
+    {
+        SceneManager.LoadScene("App_sms");
+    }
+}
