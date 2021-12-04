7c775483e371e96d649d193550c46de17e52b156
CM <corentin.mailler19@gmail.com>
2021-12-04 18:42:28 +0100

    add security screen with script in order to ask a psw and access to new app_note

---
+++ Assets/Scripts/Security.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Security : MonoBehaviour
+{
+    public GameObject changingTextError;
+    public InputField PswSelect;
+    public void ValidatePsw()
+    {
+        if (PswSelect.text == "TEST123")
+        {
+            SceneManager.LoadScene("App_note");
+        }
+        else
+        {
+            changingTextError.GetComponent<Text>().text = "Le mot de passe saisi n'est pas correct. Veuillez réessayer.";
+        }
+    }
+}
