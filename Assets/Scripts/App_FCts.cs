114505d501b1cd83f1f99f29e02bf93b982a6e6b
CM <corentin.mailler19@gmail.com>
2021-12-08 17:02:57 +0100

    creation of contacts + 'ficheContact' + call scenes

---
+++ Assets/Scripts/App_FCts.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class App_FCts : MonoBehaviour
+{
+    public void Open_FicheContact(string contactNameSelect)
+    {
+        SceneManager.LoadScene(contactNameSelect);
+    }
+
+    public void quitFCt()
+    {
+        SceneManager.LoadScene("App_sms");
+    }
+}
