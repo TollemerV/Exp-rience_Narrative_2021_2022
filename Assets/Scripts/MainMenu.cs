6dd77ae67968e86c885dd60c934ca74772765a02
CM <corentin.mailler19@gmail.com>
2022-01-27 22:31:48 +0100

    Lighting map first version

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
    public void PlayGame()
     {
-        SceneManager.LoadScene("Eliott");
+        SceneManager.LoadScene("MainScene");
     }
 
     public void QuitGame()
